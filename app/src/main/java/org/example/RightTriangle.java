72c18d2ef5864f09d0c1aa38f7e319f65fbad39a
Liam Galka <gl83991@mail.harper.college>
2025-09-28 20:20:46 -0500

    Part 1

---
+++ app/src/main/java/org/example/RightTriangle.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class RightTriangle extends Shape{
+    private double legA;
+    private double legB;
+
+    public RightTriangle(double legA, double legB) {
+        this.legA = legA;
+        this.legB = legB;
+    }
+    @Override
+    public double getArea() {
+        return 0.5 * legA * legB;
+    }
+    @Override
+    public double getPerimeter() {
+        double hypotenuse = Math.sqrt(legA * legA + legB * legB);
+        return legA + legB + hypotenuse;
+    }
+    public double getLegA() {
+        return legA;
+    }
+    public void setLegA(double legA) {
+        this.legA = legA;
+    }
+    public double getLegB() {
+        return legB;
+    }
+    public void setLegB(double legB) {
+        this.legB = legB;
+    }
+}
---
+++ app/src/main/java/org/example/Shape.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public abstract Shape {
+    public abstract double getArea();
+    public abstract double getPerimeter();
+}
