83b371ce9d19926c66cdee6fd860c1a5b83a1ffd
Liam Galka <gl83991@mail.harper.college>
2025-09-28 20:28:26 -0500

    Part 2

---
+++ app/src/main/java/org/example/IsoscelesRightTriangle.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class IsoscelesRightTriangle extends RightTriangle {
+    public IsoscelesRightTriangle(double leg) {
+        super(leg, leg);
+    }
+
+    @Override
+    public double getArea() {
+        return super.getArea();
+    }
+
+    @Override
+    public double getPerimeter() {
+        return super.getPerimeter();
+    } // Did this just incase of future problems
+    
+}
---
+++ app/src/main/java/org/example/Square.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Square extends Rectangle {
+    public Square(double side) {
+        super(side, side);
+    }
+    
+    @Override
+    public double getArea() {
+        return super.getArea();
+    }
+    @Override
+    public double getPerimeter() {
+        return super.getPerimeter();
+    } // Did this just incase of future problems
+}
